00b447b661ecaab032918c10c8e2d24099abd097
NikitaTrifonov <55878043+NikitaTrifonov@users.noreply.github.com>
2020-02-11 18:19:46 +0300

    Added checking of entered values in inputs on EditProdct page Added folder Content and css file ProductShop

--- ProductShop(empty)/Scripts/ProductScripts/AddEditProduct.js
+++ ProductShop(empty)/Scripts/ProductScripts/AddEditProduct.js
@@ -2,7 +2,7 @@
     var product = window.product;
     if (product === null) {
         $("#inputId").val("Автозаполнение");
-        $("#AddEditBtn").val("Добавить");
+        $("#submitButton").val("Добавить");
 
     }
     else {
@@ -10,31 +10,61 @@
         $("#inputName").val(product.ProductName);
         $("#inputPrice").val(product.ProductPrice);
 
-        $("#AddEditBtn").click(function () {
+        $("#submitButton").click(function () {
             product.ProductName = $("#inputName").val();
             product.ProductPrice = $("#inputPrice").val();
-
-            var prod = {
-                ProductId: product.ProductId,
-                ProductName: $("#inputName").val(),
-                ProductPrice: $("#inputPrice").val()
+            if (!$.trim(product.ProductName)) {               
+                getStatusMessage('failName');
             }
-            $.post("EditProduct", prod, succsesFunction);
-
-            //$.ajax({
-            //    type: "POST",
-            //    url: "/Product/EditProduct",
-            //    data: prod
-                
-            //});
+            else
+                if ($("#inputPrice").val() <= 0 || !$("#inputPrice").val()) {                    
+                    getStatusMessage("failPrice");
+                }
+                else {
+                    $.post("EditProduct", product,  getStatusMessage('success'));
+                }
         });
     }
-   
 }
+
 $(function () {
     init()
 });
 
-function succsesFunction() {
-    $("#succsesMessage").text("Успешно!");
+
+function setErrorColorMessage() {
+    $("#succsesMessage").removeAttr('class');
+    $("#succsesMessage").attr('class', 'errorMessage');
+}
+
+function getStatusMessage(status) {
+    switch (status) {
+        case "success":
+            $("#succsesMessage").removeAttr('class');
+            $("#succsesMessage").attr('class', 'successMessage');
+            $("#succsesMessage").text("Успешно! Изменения сохранены!");
+            break;
+        case "failName":
+            setErrorColorMessage()
+            $("#succsesMessage").text("Некорректный ввод названия продукта! Название не может быть пустым. Изменения не сохранены!");
+            break;
+        case "failPrice":
+            setErrorColorMessage();
+            $("#succsesMessage").text("Некорректный ввод цены продукта! Цена не может быть меньше или равна нулю. Изменения не сохранены!");
+            break;
+    }
+}
+
+function checkParams() {
+    if ($("#inputName").val().length != 0 && $("#inputPrice").val().length != 0) {
+        $("#submitButton").removeAttr('disabled');
+    }
+    else {
+        $("#submitButton").attr('disabled', 'disabled');
+    }
+}
+function limitDecimal(e) {
+    if (e.value.indexOf(".") != '-1') {
+        e.value = e.value.substring(0, e.value.indexOf(".") + 3);         
+    }
 }
