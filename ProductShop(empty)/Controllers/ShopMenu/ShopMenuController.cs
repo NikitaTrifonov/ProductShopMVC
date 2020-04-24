d6a155d3addb06ad23638386053996a0f25e1d2d
NikitaTrifonov <55878043+NikitaTrifonov@users.noreply.github.com>
2020-04-24 13:32:47 +0300

    added  folders and classes for ShopMenu

---
+++ ProductShop(empty)/Controllers/ShopMenu/ShopMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductShop_empty_.Controllers.ShopMenu
+{
+    public class ShopMenuController : Controller
+    {
+        // GET: ShopMenu
+        public ActionResult ShopMenuList()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProductShopMVC.Services/Models/ShopMenu/ShopMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductShopMVC.Services.Models.Products;
+
+namespace ProductShopMVC.Services.Models.ShopMenu
+{
+    class ShopMenu
+    {
+        public List<Product> shopMenu = new List<Product>();
+
+        public ShopMenu(List<Product> shopMenu)
+        {
+            this.shopMenu = shopMenu;
+        }
+    }
+}
