3e62d789cb1c003127286714bb926b30c1e8d176
NikitaTrifonov <55878043+NikitaTrifonov@users.noreply.github.com>
2020-05-04 19:47:45 +0300

    front
    get array of selected checkBoxes

--- ProductShop(empty)/Scripts/ShopMenu/ShopMenuList.js
+++ ProductShop(empty)/Scripts/ShopMenu/ShopMenuList.js
@@ -14,9 +14,9 @@ function renderCheckBoxCategories(RequstResult) {
     if (RequstResult.IsSuccess) {
         for (let i = 0; i < RequstResult.Data.length; i++) {
             if (i === 0)
-                $(".checkList").append(`<li><input type='checkbox' class='allCategories' value='${i}' onchange='checkSelect(this)' checked /><b>${RequstResult.Data[i]}</b></li>`);
+                $(".checkList").append(`<li><input type='checkbox'  class='allCategories' value='${RequstResult.Data[i]}' onchange='checkSelect(this)' /><b>${RequstResult.Data[i]}</b></li>`);
             else
-                $(".checkList").append(`<li><input type='checkbox' class='otherCategory' value='${i}' onchange='checkSelect(this)' />${RequstResult.Data[i]}</li>`);
+                $(".checkList").append(`<li><input type='checkbox'  class='otherCategory' value='${RequstResult.Data[i]}' onchange='checkSelect(this)' />${RequstResult.Data[i]}</li>`);
         }
     }
     else {
@@ -26,26 +26,40 @@ function renderCheckBoxCategories(RequstResult) {
     }
 }
 
-function checkSelect(checkBox) {
-    if (checkBox.checked === true) {
+function checkSelect(checkBox) {   
+    if (checkBox.checked === true) {      
         switch (checkBox.className) {
             case "allCategories":
+                checkBox.classList.toggle("selectedCheckBox");
                 disableCheckBoxes("otherCategory");
                 break;
             case "otherCategory":
+                checkBox.classList.toggle("selectedCheckBox");
                 disableCheckBoxes("allCategories");
                 break;
         }
     }
+    else
+        checkBox.classList.toggle("selectedCheckBox");
 }
 
 function disableCheckBoxes(checkBoxName) {
     let otherCheckBoxes = document.getElementsByClassName(checkBoxName);
     for (let i = 0; i < otherCheckBoxes.length; i++) {
+        otherCheckBoxes[i].classList.remove("selectedCheckBox");
         otherCheckBoxes[i].checked = false;
     }
 }
 
+$(".submitCategories").click(function () {
+    let checkBoxes = document.getElementsByClassName("selectedCheckBox");
+    let selectedCategories = [];
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked)
+            selectedCategories.push(checkBoxes[i].getAttribute("value"));
+    }
+    alert(selectedCategories);
+})
 
 
 function renderShopMenuItems(RequstResult) {
